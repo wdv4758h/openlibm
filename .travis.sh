9f66cd559c8a6b885acc95d150e283ebed064723
dv <wdv4758h@gmail.com>
2016-01-20 23:19:56 +0800

    add clang and `-O0` to Travis CI matrix

--- .travis.sh
+++ .travis.sh
@@ -5,11 +5,8 @@ case "$TARGET" in
 host)
 	uname -a
 	export LOADER=
-	make ;;
+	make CC=$CC ;;
 arm32)
-	sudo bash -c 'echo >> /etc/apt/sources.list "deb http://archive.ubuntu.com/ubuntu/ trusty main restricted universe"'
-	sudo apt-get update
-	sudo apt-get -y install gcc-4.7-arm-linux-gnueabihf qemu binfmt-support
 	make CC="arm-linux-gnueabihf-gcc-4.7"
 	export LD_LIBRARY_PATH=/usr/arm-linux-gnueabihf/lib
 	#export LOADER=/usr/arm-linux-gnueabihf/lib/ld-linux-armhf.so.3
